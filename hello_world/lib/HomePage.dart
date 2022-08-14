9db8977e4f915506de39782c89b4467d9ad4a1dc
Jean Bragamonte <jeanbragamonte@gmail.com>
2022-08-14 19:05:36 -0300

    state manager

--- hello_world/lib/HomePage.dart
+++ hello_world/lib/HomePage.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/HomeController.dart';
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     var controller = HomeController.of(context);
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('teste flutter'),
+    //   ),
+    //   body: Center(
+    //     child: Text('teste ${controller.value}'),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     child: Icon(Icons.add),
+    //     onPressed: (){
+    //       controller.increment();
+    //     },
+    //   ),
+    // );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +42,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('teste flutter'),
       ),
-      body: const Center(
-        child: Text('teste'),
+      body: Center(
+        child: Text('teste $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: (){
-          print('Clicou');
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
--- hello_world/lib/MyApp.dart
+++ hello_world/lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/HomeController.dart';
 import 'homePage.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,6 +7,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return MaterialApp(home: HomeController(child: HomePage()));
   }
 }
